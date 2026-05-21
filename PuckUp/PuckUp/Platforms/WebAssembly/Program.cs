dc5a81e4fff750c8ac1ed30ae6ec4ee2a803292d
Matt <90909523+mtmattei@users.noreply.github.com>
2026-05-21 18:27:51 -0400

    Delete PuckUp directory

--- PuckUp/PuckUp/Platforms/WebAssembly/Program.cs
+++
@@ -1,9 +0,0 @@
-using Uno.UI.Hosting;
-using PuckUp;
-
-var host = UnoPlatformHostBuilder.Create()
-    .App(() => new App())
-    .UseWebAssembly()
-    .Build();
-
-await host.RunAsync();
--- PuckUp/PuckUp/ViewModels/TeamItem.cs
+++
@@ -1,9 +0,0 @@
-namespace PuckUp.ViewModels;
-
-public class TeamItem
-{
-    public string Id { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-    public string League { get; set; } = string.Empty;
-    public int Players { get; set; }
-}
